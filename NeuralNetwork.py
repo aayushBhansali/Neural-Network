0d294c4ad7acdf03fc0274e3e52811b446a97923
aayushBhansali <aayushbhansali692@gmail.com>
2020-02-12 17:36:55 +0530

    Added softmax activation

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,14 +1,10 @@
 from Matrix.Matrix import Matrix
 import math
-import random
+import numpy as np
 
 class NeuralNetwork:
 
-    # A Constructor initializing input, hidden and corresponding weight matrices
     def __init__(self, inputs, hidden, outputs):
-        self.input_nodes = inputs
-        self.hidden_nodes = hidden
-        self.output_nodes = outputs
 
         self.weight_hi = Matrix(hidden, inputs)
         self.weight_oh = Matrix(outputs, hidden)
@@ -22,8 +18,20 @@ class NeuralNetwork:
         self.bias_oh.randomize()
 
         self.learning_rate = 0.1
+        self.sum = 0
+        self.max = 0
 
-    # Leaky ReLU Activation
+
+    def softmax(self, x):
+        exp = np.exp(x)
+        ans = exp/self.sum
+        return ans
+
+
+    def dsoftmax(self, x):
+        return x * (1 - x)
+
+
     @staticmethod
     def ReLU(x):
         if x > 0:
@@ -31,7 +39,7 @@ class NeuralNetwork:
         else:
             return 0.1 * x
 
-    # Derivative of Leaky ReLU
+
     @staticmethod
     def dReLU(y):
         if y > 0:
@@ -40,11 +48,12 @@ class NeuralNetwork:
             return 0.1
 
 
-    # Activation Function - Sigmoid
+
     @staticmethod
     def sigmoid(x):
-        fire = 1 /float(1.0 + math.exp(-x))
+        fire = 1 /float(1.0 + np.exp(-x))
         return fire
+
 
     @staticmethod
     def dsigmoid(y):
@@ -52,93 +61,77 @@ class NeuralNetwork:
         return der
 
 
-    # Feeds the input throughout the network
-    def feedforward(self, inputs):
 
+    def feedforward(self, inputs):
+        self.sum = 0
+        self.max = 0
         input = Matrix.fromArray(inputs)
 
-        # Obtain the Output from the first layer
-        # This will be the input to the hidden layer
+
         hidden_op = self.weight_hi.multiply(input)
-        # hidden_op.display()
-
-        # Add the bias
         hidden_op.add(self.bias_ih)
+        hidden_op = Matrix.map(hidden_op, self.sigmoid)
 
-        # Pass through the sigmoid activation function
-        hidden_op = Matrix.map(hidden_op, self.ReLU)
-
-        # Calculate final output
         final_op = self.weight_oh.multiply(hidden_op)
-
-        # Add bia to the output
         final_op.add(self.bias_oh)
+        final_op = Matrix.map(final_op, self.scale) 
 
-        # Pass through sigmoid activation function
-        final_op = Matrix.map(final_op, self.ReLU)
+        for i in range(final_op.rows):
+            for j in range(final_op.columns):
+                self.sum += np.exp(final_op.matrix[i][j])
 
+        final_op = Matrix.map(final_op, self.softmax)
         op = Matrix.toArray(final_op)
         return op
 
+
+    def scale(self, x):
+        return x
+
+
     def train(self, inputs, outputs, lr):
+        self.sum = 0
         self.learning_rate = lr
         input = Matrix.fromArray(inputs)
 
-        # Obtain the Output from the first layer
-        # This will be the input to the hidden layer
         hidden_ip = self.weight_hi.multiply(input)
-
-        # Add bias to it
         hidden_ip.add(self.bias_ih)
 
-        # Sigmoid activation function
-        hidden_op = Matrix.map(hidden_ip, self.ReLU)
+        hidden_op = Matrix.map(hidden_ip, self.sigmoid)
 
-        # Obtain Final Output of NN
         final_op = self.weight_oh.multiply(hidden_op)
-
-        # Add Bias into it
         final_op.add(self.bias_oh)
+        final_op = Matrix.map(final_op, self.scale) 
 
-        # Sigmoid Activation function
-        final_op = Matrix.map(final_op, self.ReLU)
+        for i in range(final_op.rows):
+            for j in range(final_op.columns):
+                self.sum += np.exp(final_op.matrix[i][j])
+        
+        final_op = Matrix.map(final_op, self.softmax)
+        final_op.display()
 
-        # fetch Output
         output = Matrix.fromArray(outputs)
         output = output.transpose()
 
-        # Calculate error
         error = Matrix.subtract_stat(output, final_op)
-
-        # Calculate Gradient
-        gradient = Matrix.map(final_op, self.dReLU)
+        gradient = Matrix.map(final_op, self.dsoftmax)
         gradient = gradient.element_wise_multiply(error)
         gradient.scalar(self.learning_rate)
+
         self.bias_oh.add(gradient)
-
         hidden_op_t = hidden_op.transpose()
-
-        # Calculate Delta Weights
         weight_deltas = gradient.multiply(hidden_op_t)
-
-        # Update Current Weights
         self.weight_oh.add(weight_deltas)
-        who_t = self.weight_oh.transpose()
 
+        who_t = self.weight_oh.transpose()
         error = error.transpose()
         hidden_error = who_t.multiply(error)
 
-        # Calculate Gradient between Hidden Layer and Input Layer Weights
-        hidden_gradient = Matrix.map(hidden_op, self.dReLU)
+        hidden_gradient = Matrix.map(hidden_op, self.dsigmoid)
         hidden_gradient = hidden_gradient.element_wise_multiply(hidden_error)
         hidden_gradient.scalar(self.learning_rate)
-
         self.bias_ih.add(hidden_gradient)
 
         input_t = input.transpose()
-
-        # Calculate delta weights
         weight_deltas_ih = hidden_gradient.multiply(input_t)
-
-        # Update Current weights
         self.weight_hi.add(weight_deltas_ih)
